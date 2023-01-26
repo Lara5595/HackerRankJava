15bdf682165ffcd59daf4f4d6424ffe5e31a5dd3
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2023-01-26 17:45:23 -0600

    hello world

---
+++ JavaTraining/src/HackerRank.java
@@ -0,0 +1,12 @@
+public class HackerRank {
+    //  Question 1 System.out.println("Hello World")
+
+
+
+    public static void main(String[] args) {
+        
+        // question 1 answer
+        System.out.println("Hello World.");
+
+    }
+}

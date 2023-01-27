1b064af6ffe03b886ca96893bcecca8c0032e373
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2023-01-26 18:27:43 -0600

    finish question 2

--- JavaTraining/src/HackerRank.java
+++ JavaTraining/src/HackerRank.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class HackerRank {
     //  Question 1 System.out.println("Hello World")
 
+    // Question 2 Java Stdin and Stdout 1
 
 
     public static void main(String[] args) {
         
         // question 1 answer
-        System.out.println("Hello World.");
+        // System.out.println("Hello World.");
+
+        // question 2 answer
+        Scanner scanner = new Scanner(System.in);
+        String myString = scanner.next();
+        
+        int myInt = scanner.nextInt();
+        // scanner.close();
+        
+        System.out.println("myString is: " + myString);
+        System.out.println("myInt is: " + myInt);
+
+
+
+
+
+
 
     }
 }

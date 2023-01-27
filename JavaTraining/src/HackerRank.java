1ba87902e9fd1e4667a31e358b423babaebd6cb5
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2023-01-26 19:08:56 -0600

    finish question 3

--- JavaTraining/src/HackerRank.java
+++ JavaTraining/src/HackerRank.java
@@ -5,6 +5,13 @@ public class HackerRank {
 
     // Question 2 Java Stdin and Stdout 1
 
+    // Question 3 If else statement Given an integer, , perform the following conditional actions:
+    // If  is odd, print Weird
+    // If  is even and in the inclusive range of  to , print Not Weird
+    // If  is even and in the inclusive range of  to , print Weird
+    // If  is even and greater than , print Not Weird
+    // Complete the stub code provided in your editor to print whether or not  is weird.
+
 
     public static void main(String[] args) {
         
@@ -13,17 +20,27 @@ public class HackerRank {
 
         // question 2 answer
         Scanner scanner = new Scanner(System.in);
-        String myString = scanner.next();
+        // String myString = scanner.next();
         
-        int myInt = scanner.nextInt();
-        // scanner.close();
+        // int myInt = scanner.nextInt();
+        // // scanner.close();
         
-        System.out.println("myString is: " + myString);
-        System.out.println("myInt is: " + myInt);
+        // System.out.println("myString is: " + myString);
+        // System.out.println("myInt is: " + myInt);
 
-
-
-
+        // question 3 answer
+        
+        int N = scanner.nextInt();
+        if(N %2 != 0) {
+            System.out.println("Weird");
+        } else if(N %2 == 0 && N <= 5 && N >= 2 ){
+            System.out.println("Not Weird");
+        } else if(N %2 == 0 && N <= 20 && N >=6 ){
+            System.out.println("Weird");   
+        } else if(N %2 == 0 && N >= 20) {
+            System.out.println("Not Weird");
+        }
+        
 
 
 
